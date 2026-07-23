7910cf27eb25577174d878a3975bc91257628f84
Moza <mozaalmamri16@gmail.com>
2026-07-23 10:17:43 +0400

    Create the department table

--- DataBaseTasks/SQLQuery1.sql
+++ DataBaseTasks/SQLQuery1.sql
@@ -1,5 +1,15 @@
+/*
 CREATE DATABASE CompanyDB;
 GO
 
 USE CompanyDB;
 GO
+*/
+
+CREATE TABLE Department
+(
+    Dnumber INT PRIMARY KEY,
+    Dname VARCHAR(50) NOT NULL UNIQUE,
+    Mgr_ssn CHAR(9),
+    Mgr_start_date DATE
+);
