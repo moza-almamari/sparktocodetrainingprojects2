2d96b8a05c8b33f15e2bdc168809a8860fd206f2
Moza <mozaalmamri16@gmail.com>
2026-07-28 21:38:33 +0400

    Category class

---
+++ E-Commerce_Database/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce_Database.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
