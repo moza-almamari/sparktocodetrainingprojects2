1ebe06b850bc6522f9e52b6371cdefd18cd25217
Moza <mozaalmamri16@gmail.com>
2026-07-28 21:41:57 +0400

    Review class

---
+++ E-Commerce_Database/Models/Review.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce_Database.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public int Rating { get; set; }   
+        public string Comment { get; set; }
+    }
+}
