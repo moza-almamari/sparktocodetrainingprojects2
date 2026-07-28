4ed5ecd3f57dc595bfde6cb0182171f1554b630f
Moza <mozaalmamri16@gmail.com>
2026-07-28 21:35:54 +0400

    user model class

---
+++ E-Commerce_Database/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce_Database.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        
+    }
+}
---
+++ E-Commerce_Database/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Commerce_Database
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
