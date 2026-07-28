39785ae8889577cce06edfb4c4ccb727758d8e79
Moza <mozaalmamri16@gmail.com>
2026-07-28 21:40:58 +0400

    Order class

---
+++ E-Commerce_Database/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce_Database.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+    }
+}
