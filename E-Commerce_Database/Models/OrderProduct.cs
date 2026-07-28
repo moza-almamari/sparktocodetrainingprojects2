eecaa1e679a5cdee27404a1310475b43b98847d2
Moza <mozaalmamri16@gmail.com>
2026-07-28 21:43:01 +0400

    OrderProduct class

---
+++ E-Commerce_Database/Models/OrderProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce_Database.Models
+{
+    public class OrderProduct
+    {
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
