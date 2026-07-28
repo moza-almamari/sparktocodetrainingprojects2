92a59563239daffb2bb24c47de35ffe912dc150a
Moza <mozaalmamri16@gmail.com>
2026-07-28 21:39:51 +0400

    Product class

---
+++ E-Commerce_Database/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce_Database.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
